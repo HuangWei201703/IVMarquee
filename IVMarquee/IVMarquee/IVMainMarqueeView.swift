df9b149e473c16f079704a94dd2baafd0926a34a
IvanLee <17090316311@163.com>
2017-10-13 11:25:49 +0800

    initialize

---
+++ IVMarquee/IVMarquee/IVMainMarqueeView.swift
@@ -0,0 +1,23 @@
+//
+//  IVMainMarqueeView.swift
+//  IVMarquee
+//
+//  Created by IvanLee on 2017/10/13.
+//  Copyright © 2017年 lifan666. All rights reserved.
+//
+
+import UIKit
+
+class IVMainMarqueeView: UIView {
+
+    init(frame: CGRect, titleArray: [String]) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ IVMarquee/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IVMarquee
+//
+//  Created by IvanLee on 2017/10/13.
+//  Copyright © 2017年 lifan666. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
